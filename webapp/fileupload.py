669e51a48969ea04c629f246cad1ebfcef16a5c3
OlaOst <olaa81@gmail.com>
2013-10-12 18:21:03 +0000

    More improved fileupload

--- webapp/fileupload.py
+++ webapp/fileupload.py
@@ -4,8 +4,7 @@ from bottle import get, post, request, run
 
 uploadform = '''
     <form action="/upload" method="post" enctype="multipart/form-data">
-        Upload file: <input name="name" type="text" />
-                     <input name="data" type="file" />
+        Upload file: <input name="data" type="file" />
                      <input value="upload" type="submit" />
     </form>
 '''
@@ -16,14 +15,14 @@ def upload():
 
 @post('/upload') # or @route('/login', method='POST')
 def do_upload():
-    name = request.forms.name
+    #name = request.forms.name
     data = request.files.data
 
-    if name and data and data.file:
+    if data and data.file:
         raw = data.file.read() # Dangerous for big files
         filename = data.filename
         report = "<p>The uploaded file named " + filename + " is " + str(len(raw)) + " bytes long</p>"
-        report += "<p>Raw file contents are:</p><p>" + raw + "</p>"
+        report += "<p>Raw file contents are:</p><pre>" + raw + "</pre>"
     else:
         report = "<p>You missed a field or there was a problem uploading the file.</p>"
 
