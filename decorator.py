2ee3a01ea0f8afd0a4d6e73834f277b3af01befe
OlaOst <olaa81@gmail.com>
2013-10-12 09:50:35 +0000

    Implement decorator example

--- decorator.py
+++ decorator.py
@@ -1,1 +1,32 @@
 # Memoize decorator - a decorator that caches expensive computations
+# Shamelessly copied from https://wiki.pthon.org/moin/PythonDecoratorLibrary
+
+import collections
+
+# TODO: make sure that different functions with equal parameters are cached separately
+class memoized(object):
+    '''Decorator that caches the return value of a function when it is called.
+       If called later with the same arguments the cached value is returned
+    '''
+    def __init__(self, func):
+        self.func = func
+        self.cache = {}
+    def __call__(self, *args):
+        if not isinstance(args, collections.Hashable):
+            # if args are not hashable forget trying to cache it, just call the function normally
+            return self.func(*args)
+        if args in self.cache:
+            return self.cache[args]
+        else:
+            value = self.func(*args)
+            self.cache[args] = value
+            return value
+
+@memoized
+def fibonacci(n):
+    "Return the nth fibonacci number recursively"
+    if n in (0, 1):
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print fibonacci(35)
