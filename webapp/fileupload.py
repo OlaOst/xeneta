3f7b0c454b13638151e08c4c18f47064218df766
OlaOst <olaa81@gmail.com>
2013-10-12 18:17:32 +0000

    Make fileupload see the filecontents

--- webapp/fileupload.py
+++ webapp/fileupload.py
@@ -3,9 +3,10 @@ from bottle import get, post, request, run
 
 
 uploadform = '''
-    <form action="/upload" method="post">
-        Upload file: <input name="file" type="file" />
-        <input value="upload" type="submit" />
+    <form action="/upload" method="post" enctype="multipart/form-data">
+        Upload file: <input name="name" type="text" />
+                     <input name="data" type="file" />
+                     <input value="upload" type="submit" />
     </form>
 '''
 
@@ -15,10 +16,17 @@ def upload():
 
 @post('/upload') # or @route('/login', method='POST')
 def do_upload():
-    filedata = request.forms.get('file')
-    report = "<p>The uploaded file is " + str(len(filedata)) + " bytes long</p>"
-    report += "<p>File contents are:</p><p>" + filedata + "</p>"
-    report +="<p>Request forms keys are: " + str(request.forms.keys()) + "</p>"
+    name = request.forms.name
+    data = request.files.data
+
+    if name and data and data.file:
+        raw = data.file.read() # Dangerous for big files
+        filename = data.filename
+        report = "<p>The uploaded file named " + filename + " is " + str(len(raw)) + " bytes long</p>"
+        report += "<p>Raw file contents are:</p><p>" + raw + "</p>"
+    else:
+        report = "<p>You missed a field or there was a problem uploading the file.</p>"
+
     return uploadform + report
 
 run(host='0.0.0.0', port=8080)
