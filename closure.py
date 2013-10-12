711393a2152bfa5be31097ecee793c8311705ac4
OlaOst <olaa81@gmail.com>
2013-10-12 09:26:17 +0000

    Make simple closure example

--- closure.py
+++ closure.py
@@ -1,3 +1,11 @@
 # A closure is a nested function that can access variables that have gone out of scope when it is called
 
+# TODO: make more meaningful example
+def makeClosure(text):
+    def closure():
+        print text
+    return closure
 
+myClosure = makeClosure("hello world")
+myClosure() 
+# at this point the text variable in the makeClosure function is out of scope, but myClosure can still access it - it 'encloses' the scope or context it was defined in
