b310269d1f74c17b1254942d40d01719be83aac4
OlaOst <olaa81@gmail.com>
2013-10-12 18:50:20 +0000

    displayfiles now shows 20 most popular words

--- webapp/displayfiles.py
+++ webapp/displayfiles.py
@@ -13,6 +13,13 @@ def display():
 
     wordcount = parsefiles(files)
 
-    return str(wordcount)
+    sortedwords = sorted(wordcount, key=wordcount.get, reverse=True)
+
+    result = "<p>20 most popular words:</p><p>"
+    for word in sortedwords[:20]:
+        result += word + ": " + str(wordcount[word]) + "<br/>"
+
+    #return str(wordcount)
+    return result
 
 run(host='0.0.0.0', port=8080)
