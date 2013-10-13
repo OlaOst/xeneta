a79d86f84bf217c2523956b63fd5799dc7a63714
OlaOst <olaa81@gmail.com>
2013-10-13 09:42:07 +0000

    Display list of files loaded

--- webapp/displayfiles.py
+++ webapp/displayfiles.py
@@ -6,17 +6,15 @@ def display():
     # TODO: time this for large files and with multiple simultaneous connections and optimize if needed
     files = getfilelist()
 
-    print "got files: " + str(files)
+    #print "got files: " + str(files)
+    result = "<p>Files loaded: " + ", ".join(files) + "</p>"
 
     wordcount = parsefiles(files)
 
     sortedwords = sorted(wordcount, key=wordcount.get, reverse=True)
 
-    result = "<p>20 most popular words:</p><p>"
+    result += "<p>20 most popular words:</p><p>"
     for word in sortedwords[:20]:
         result += word + ": " + str(wordcount[word]) + "<br/>"
 
-    #return str(wordcount)
     return result
-
-#run(host='0.0.0.0', port=8080)
