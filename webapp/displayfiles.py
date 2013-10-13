d8994a94bb499bc30b0c040e510972527d80b3fd
OlaOst <olaa81@gmail.com>
2013-10-13 10:25:40 +0000

    Add javascript files and views template

--- webapp/displayfiles.py
+++ webapp/displayfiles.py
@@ -1,13 +1,17 @@
-from bottle import get, run
+from bottle import route, view, run
 from fileutil import getfilelist, parsefiles
 
-@get('/display')
+@route('/display')
+@view('display')
 def display():
+    result = {}
+
     # TODO: time this for large files and with multiple simultaneous connections and optimize if needed
     files = getfilelist()
 
     #print "got files: " + str(files)
-    result = "<p>Files loaded: " + ", ".join(files) + "</p>"
+    #result = "<p>Files loaded: " + ", ".join(files) + "</p>"
+    result['files'] = ", ".join(files)
 
     # TODO: make global wordcount so we do not have to recalculate it from all files on every request
     wordcount = {}
@@ -15,8 +19,11 @@ def display():
 
     sortedwords = sorted(wordcount, key=wordcount.get, reverse=True)
 
-    result += "<p>20 most popular words:</p><p>"
+    #result += "<p>20 most popular words:</p><p>"
+    topwords = ""
     for word in sortedwords[:20]:
-        result += word + ": " + str(wordcount[word]) + "<br/>"
+        topwords += word + ": " + str(wordcount[word]) + "<br/>"
+
+    result['topwords'] = sortedwords[:20]
 
     return result
---
+++ webapp/views/display.tpl
@@ -0,0 +1,10 @@
+<html>
+    <head>
+      <script>js/jquery-2.0.3.min.js</script>
+    </head>
+    <body>
+        <p>Files loaded: {{files}}</p>
+	<p>20 most common words:</p>
+	{{topwords}}
+    </body>
+</html>
