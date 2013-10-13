3c83e6037169bfdd50f4306d3c9873df55cee6d9
OlaOst <olaa81@gmail.com>
2013-10-13 14:06:17 +0000

    Cleanup fileutils

--- webapp/fileutil.py
+++ webapp/fileutil.py
@@ -11,17 +11,14 @@ def getfilelist():
     return files
 
 def parsefiles(files, wordcount):
-    #wordcount = {}
     for filename in files:
-        #print "loading file " + filename
         parsefile(filename, wordcount)
 
-# TODO: might be better performance to split the whole file instead of every line separately
 def parsefile(filename, wordcount):
-    lines = file("./files/" + filename, 'r').readlines()
-    for line in lines:
-        for word in line.split():
-            if word in wordcount:
-                wordcount[word] += 1
-            else:
-                wordcount[word] = 1
+    content = file("./files/" + filename, 'r').read()
+
+    for word in content.split():
+        if word in wordcount:
+            wordcount[word] += 1
+        else:
+            wordcount[word] = 1
